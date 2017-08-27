7bbe903e7b7b3c784a12b86fea08a9c19799ce69
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-08-27 14:09:00 -0400

    attempt merging versions of strings

--- comparevitae/textutil.py
+++ comparevitae/textutil.py
@@ -14,6 +14,9 @@ from nltk.corpus import wordnet as wn
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
 
+lemma = WordNetLemmatizer()
+stemmer = PorterStemmer()
+
 
 def penn_to_wn(tag):
 
@@ -31,7 +34,6 @@ def penn_to_wn(tag):
 
 def normalize_text(content, ignore_words=[]):
 
-    lemma = WordNetLemmatizer()
     norm_text = content.encode("ascii", "ignore").lower()
 
     for punc in punctuation:
@@ -66,32 +68,41 @@ def normalize_text(content, ignore_words=[]):
     return " ".join(filter(None, time_filtered))
 
 
-def find_index(word, normalized_sent, sentence):
+def find_index(word, normalized_sent, sentence, score):
 
-    sentence = "Certificate in Data Science"
-    normalized_sent = "certificate in data science"
-    word = "data science"
+    # word = "expect"
+    # sentence = "| EXPECTED: MAY 2018"
+    # normalized_sent = "expect"
 
+    # print "\n", word, "<=>", normalized_sent, "<=>", sentence
     if len(sentence) == len(normalized_sent):
         start = normalized_sent.find(word)
         end = start + len(word)
         return sentence.replace(
-            sentence[start:end], sentence[start:end] + "YOOOOOO")
+            sentence[start:end],
+            "<div style=\"{}\">".format(score) + sentence[start:end] + "</div>"
+        )
 
-    stemmer = PorterStemmer()
-    re_pat = " ".join(
+    re_str = " ".join(
         stemmer.stem(i) for i in word.split()
-    ).replace(" ", ".*") + "[^|\s]+"
-    regex = re.compile(re_pat, re.I | re.M)
-    return sentence.replace(
-        regex.findall(sentence)[0],
-        regex.findall(sentence)[0] + "YOOOOOOO"
-    )
+    ).replace(" ", ".*") + "([.-]|[^\s]+|[^\s]*?)"
+    regex = re.compile(re_str, re.I)
+    matches = regex.search(sentence)
+    # print(re_str)
+    try:
+        return sentence.replace(
+            matches.group(),
+            "<div style=\"{}\">".format(score) + matches.group() + "</div>"
+        )
+    except AttributeError:
+        print "failed \x1b[3;31;40m" + re_str + "\x1b[0m"
 
 
 def recreate_doc(tfidf_scores_path, parsed_html):
 
     tfidf_scores = {}
+    FAILED = 0
+    SUCCESS = 0
 
     with open(tfidf_scores_path) as tfidf_scores_file:
         tfidf_scores = json.load(tfidf_scores_file)
@@ -104,8 +115,15 @@ def recreate_doc(tfidf_scores_path, parsed_html):
     for p_tag in parsed_soup.find_all("p"):
         for term in tfidf_terms:
             if term in normalize_text(p_tag.text):
-                print term, tfidf_terms[term], "<=>", normalize_text(p_tag.text), "<=>", p_tag.text
+                x = find_index(term, normalize_text(p_tag.text),
+                               p_tag.text, tfidf_terms[term])
+                print x
+                if not x:
+                    FAILED += 1
+                else:
+                    SUCCESS += 1
+                # print term, tfidf_terms[term], "<=>", normalize_text(p_tag.text), "<=>", p_tag.text
                 # p_tag.string = "YOOOOOOOOOOOO"
-
+    print float(FAILED) / SUCCESS * 100
     # print(parsed_soup.find_all("p"))
     # return " ".join(i.text for i in parsed_soup.findAll("p"))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import, unicode_literals
 
 from comparevitae.metrics import RateResume
 from comparevitae.textutil import recreate_doc, find_index
@@ -18,6 +18,49 @@ if __name__ == "__main__":
     # obj.generate_tfidf(stop_words="english")
     # obj.get_score()
     # recreate_doc()
-    # x = pdf_to_html("sample/sabbir1.pdf")
-    # recreate_doc(tfidf_scores_path="resume_tfidf.json", parsed_html=x)
-    print(find_index(1, 2, 3))
+    x = pdf_to_html("sample/sabbir1.pdf")
+    recreate_doc(tfidf_scores_path="resume_tfidf.json", parsed_html=x)
+    # print(find_index(1, 2, 3))
+
+    from textwrap import dedent
+    TEST = dedent("""
+        • Best Education Hack winning web application that predicted the discipline of the user’s <div style="0.016160527539">school</div> curriculums
+        • <div style="0.04063447037">Best Education Hack</div> winning web application that predicted the discipline of the user’s school curriculums
+        • Best Education <div style="0.0812689407401">Hack</div> winning web application that predicted the discipline of the user’s school curriculums
+        • Best <div style="0.04063447037">Education Hack</div> winning web application that predicted the discipline of the user’s school curriculums
+        • Best Education Hack winning web application that predicted the discipline of the <div style="0.0211116784262">user’s</div> school curriculums
+        • Best Education Hack winning web application that predicted the discipline of the user’s school <div style="0.0252122523599">curriculums</div>
+        • Best Education Hack winning web application that <div style="0.0470029256762">predicted</div> the discipline of the user’s school curriculums
+        • Best Education Hack winning web application that predicted the discipline of the user’s <div style="0.0372937480598">school curriculums</div>
+        • Best Education Hack winning web application that predicted the discipline of the <div style="0.04063447037">user’s school</div> curriculums
+        • <div style="0.0230019623321">Best</div> Education Hack winning web application that predicted the discipline of the user’s school curriculums
+        • Best Education Hack winning <div style="0.0749074302973">web</div> application that predicted the discipline of the user’s school curriculums
+        • Best Education Hack winning <div style="0.0776152291155">web application</div> that predicted the discipline of the user’s school curriculums
+        • Best Education Hack winning web application that predicted the discipline of the <div style="0.0864547991075">user’s</div> school curriculums
+        • <div style="0.04063447037">Best Education</div> Hack winning web application that predicted the discipline of the user’s school curriculums
+        • Best Education Hack winning web application that predicted the <div style="0.0303126618726">discipline</div> of the user’s school curriculums
+        • Best Education Hack winning web application that predicted the discipline of the <div style="0.04063447037">user’s school curriculums</div>
+        • Best <div style="0.0433258613599">Education</div> Hack winning web application that predicted the discipline of the user’s school curriculums
+        • Best Education Hack winning web <div style="0.0629939197256">application</div> that predicted the discipline of the user’s school curriculums
+    """)
+
+import re
+
+div_str = "<div style=.*</div>"  # the div tags
+div_text_str = "(?<=(>)).*(?=(</div>))"  # the content inside the div tags
+
+# compile the regexes
+div_regex = re.compile(div_str)
+div_text_regex = re.compile(div_text_str)
+
+
+def merge_strings(str1, str2):
+
+    # grab the div tag off the first version
+    div = div_regex.search(str1).group()
+    # grab the contents of that div tag
+    div_text = div_text_regex.search(div).group()
+
+    # find the div content in the second version, then substitute
+    # with the div tag
+    return re.sub(div_text, div, str2)
