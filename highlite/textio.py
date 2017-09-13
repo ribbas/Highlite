bc26cb315876fd2336ddd7883c1bd72cb3c71caf
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-09-13 17:43:48 -0400

    add hardcoded corpus for testing

--- highlite/textio.py
+++ highlite/textio.py
@@ -23,6 +23,7 @@ def pdf_to_text(file_path, parsed_path=PARSED_PATH):
     print("output:", parsed_path)
 
     print(path.exists(path.dirname(parsed_path)), "yayyyy")
+    print(lsfile(path.dirname(parsed_path), "*"))
     print(lsfile(path.dirname(parsed_path), "data", "rawcorpus", "test_corpus", "*"))
 
     subprocess.call(["pdftotext", file_path, parsed_path])
